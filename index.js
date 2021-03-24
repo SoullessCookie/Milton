d957ca83784c49cf8b4e25739982472d8ac27b0f
SoullessCookie <71998907+SoullessCookie@users.noreply.github.com>
2021-03-24 10:45:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
+// const config = require('./config.json')
 
 client.on('ready', () =>{
     console.log('Milton is ready to go!')
 })
 
-client.login(config.token)
+client.login(process.env.BOT_TOKEN)
